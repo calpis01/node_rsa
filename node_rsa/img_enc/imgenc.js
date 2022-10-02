99a0e8ae9d9a209eeeb086191b75c4f44536288b
takuro-ohashi <takuo1515@gmail.com>
2022-10-02 16:29:58 +0900

    decrypt成功

--- node_rsa/img_enc/imgenc.js
+++ node_rsa/img_enc/imgenc.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 var rsab = require('./main2');
+const NodeRSA = require('node-rsa');
 
 // 別途用意した画像を保存してくれるやつ
 var canvas_saver = require('./canvas_saver.js');
@@ -19,29 +20,29 @@ fs.readFile(__dirname + '/sample.png', function(err, data){
     ctx.drawImage(img, 0, 0, img.width, img.height);
 
 
-    var buffer = new Uint8Array(10);
+    //var buffer = new Uint8Array(10);
     // RGBの画素値の配列を取得
     var imagedata = ctx.getImageData(0, 0, img.width, img.height);
-    console.log(imagedata);
-    const normalArray = Array.from(imagedata);
-
-    var key = rsab.generateKeyPair(1024);
+    console.log(imagedata.data);
+    const normalArray = Array.prototype.slice.call(imagedata.data);
+    console.log(normalArray);
+    
+    //var key = rsab.generateKeyPair(1024);
     //console.log('private=', key.private);
     //console.log('public=', key.public);
+    const key = new NodeRSA().generateKeyPair();
+    const input = Buffer.from('test', 'utf8');
 
-    var enc2 = rsab.publicEncrypt(key.public, normalArray);
-    console.log('enc2=', enc2);
-    var dec2 = rsab.privateDecrypt(key.private, enc2);
-    console.log('OK? dec2=', dec2);
+    const encrypted = key.encrypt(normalArray);
+    fs.writeFile("file1.txt", encrypted.toString('base64'), (err) => {
+        if (err) throw err;
+        console.log('正常に書き込みが完了しました');
+      });
 
-    
-    /*
-    const byteCharacters = atob(enc2);
-    const byteNumbers = new Array(byteCharacters.length);
-    for (let i = 0; i < byteCharacters.length; i++) {
-        byteNumbers[i] = byteCharacters.charCodeAt(i);
-    }
-    const byteArray = new Uint8ClampedArray(byteNumbers);
-    console.log('uintArray=', byteArray);
-    */
+    const decrypted = key.decrypt(encrypted);
+    fs.writeFile("file2.txt", decrypted, (err) => {
+        if (err) throw err;
+        console.log('正常に書き込みが完了しました');
+      });
+
 });
